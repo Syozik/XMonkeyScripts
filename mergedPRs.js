4ebea23cc87f571f3584b9b4a2b021cef3400884
Serhii Rubanskyi <seru@odoo.com>
2026-09-04 15:56:03 +0200

    update runbot closed bundles script

--- mergedPRs.js
+++ mergedPRs.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      1.0
 // @description  Indicate PRs merged.
-// @author       BVR
+// @author       SERU (fork from BVR)
 // @match        https://runbot.odoo.com/runbot/*?search=*
 // @icon         https://cdn-icons-png.flaticon.com/512/1647/1647447.png
 // @grant        none
@@ -15,9 +15,9 @@
         const cell = el.closest('.cell');
         if (!cell) return;
 
-        const oneLine = cell.querySelector('.one_line');
-        if (oneLine && !oneLine.classList.contains('line-through')) {
-            oneLine.classList.add('line-through');
+        const text = cell.querySelector('b');
+        if (text && !text.classList.contains('line-through')) {
+            text.classList.add('line-through');
         }
     });
 })();
